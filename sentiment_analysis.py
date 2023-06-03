b560b789d07599daba6affaef2a6df08f3a9cd49
NerdyBurner <133992676+NerdyBurner@users.noreply.github.com>
2023-06-03 12:40:00 -0600

    Update sentiment_analysis.py
    Updated 6.3.23 to insure compatibility with rest of scripts
    
    Signed-off-by: NerdyBurner <133992676+NerdyBurner@users.noreply.github.com>

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,7 +1,8 @@
+#Build as of 6.3.23
 import nltk
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import openai
-from config import Config
+import logging
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from typing import Any, Dict, List, Union
 
 class ChatCompletionResponse:
@@ -11,30 +12,35 @@ class ChatCompletionResponse:
         self.choices = kwargs.get('choices', [])
 
 class SentimentAnalysis:
-    def __init__(self):
+    def __init__(self, openai_api_key: str):
         nltk.download('vader_lexicon')
         self.vader = SentimentIntensityAnalyzer()
-        openai.api_key = Config.OPENAI_API_KEY
+        openai.api_key = openai_api_key
+        # Set up logging
+        logging.basicConfig(filename='sentiment_analysis.log', level=logging.ERROR)
 
-    def analyze_vader(self, text):
-        scores = self.vader.polarity_scores(text)
-        return scores['compound']
+    def analyze_vader(self, text: str) -> float:
+        try:
+            scores = self.vader.polarity_scores(text)
+            return scores['compound']
+        except Exception as e:
+            logging.error(f"Error analyzing text with Vader: {str(e)}")
+            return 0  # Return neutral sentiment score in case of error
 
-def analyze_openai(self, text):
-    try:
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", 
-            messages=[
-                {"role": "system", "content": "This is a sentiment analysis task. Please categorize the following news article's sentiment as 'Good', 'Bad', or 'Unknown'."},
-                {"role": "user", "content": text}
+    def analyze_openai(self, text: str) -> str:
+        try:
+            response = openai.ChatCompletion.create(
+                model="text-curie-0001", 
+                messages=[
+                    {"role": "system", "content": "This is a sentiment analysis task. Please categorize the following news article's sentiment as 'Good', 'Bad', or 'Unknown'."},
+                    {"role": "user", "content": text}
                 ]
             )
-        if 'choices' in response and len(response.choices) > 0: # type: ignore \
-            # Pylance is confused by there not being a 'choices' list, or that the list is empty. \
-            # Ignoring it for now and focusing on adding core functionality as template \
-            return response.choices[0].message['content'] # type: ignore \
-        else:
-            return 'API returned no \'Choices\' or the list itself was empty.'
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-        return 'Error'
+            if 'choices' in response and len(response.choices) > 0: 
+                return response.choices[0].message['content'] 
+            else:
+                logging.error("OpenAI API returned no choices or the list itself was empty.")
+                return 'Unknown'
+        except Exception as e:
+            logging.error(f"Error analyzing text with OpenAI: {str(e)}")
+            return 'Unknown'  # Return 'Unknown' sentiment in case of error
