fdaa0521fb69448460432cce961d48be143e961b
NerdyBurner <133992676+NerdyBurner@users.noreply.github.com>
2023-06-03 12:50:47 -0600

    Update Main.py
    Updated to include all scripts and functions
    
    Signed-off-by: NerdyBurner <133992676+NerdyBurner@users.noreply.github.com>

--- Main.py
+++ Main.py
@@ -1,24 +1,70 @@
+#Build as of 6.3.23
+from dotenv import load_dotenv
 from api import Api
-from sentiment_analysis import SentimentAnalysis
 from database import Database
+from sentiment_analysis import SentimentAnalysis
+from config import Config
 
-# Instantiate the classes
-api = Api()
-sentiment_analysis = SentimentAnalysis()
-database = Database()
+load_dotenv()
 
-# Fetch the stock tickers
-tickers = api.fetch_tickers()
+def main():
+    # Initialize the Api, Database, and SentimentAnalysis classes
+    api = Api(Config.POLYGON_API_KEY, Config.TICKERS_FILE)
+    database = Database(Config.DATABASE_NAME)
+    sentiment_analysis = SentimentAnalysis(Config.OPENAI_API_KEY)
 
-# For each ticker, fetch the related news articles
-for ticker in tickers:
-    news = api.fetch_news(ticker)
-    
-    # For each news article, perform sentiment analysis
-    for article in news:
-        sentiment_score_vader = sentiment_analysis.analyze_vader(article['summary'])
-        sentiment_score_openai = sentiment_analysis.analyze_openai(article['summary'])
+    # Fetch the list of tickers
+    tickers = api.fetch_tickers()
 
-        # Save the article and sentiment scores to the database
-        database.save_article(article)
-        database.save_sentiment({'ticker': ticker, 'score_vader': sentiment_score_vader, 'score_openai': sentiment_score_openai})
+    # Process tickers in batches of 25
+    for i in range(0, len(tickers), 25):
+        batch_tickers = tickers[i:i+25]
+
+        for ticker in batch_tickers:
+            try:
+                # Fetch and save financial data
+                rsi = api.fetch_rsi(ticker)
+                macd = api.fetch_macd(ticker)
+                prices = api.fetch_prices(ticker)
+                database.save_financial_data(ticker, rsi, macd, prices)
+
+                # Initialize sentiment score counters
+                vader_score_sum = 0
+                openai_score_sum = 0
+                num_articles = 0
+
+                # Fetch news and perform sentiment analysis
+                news_articles = api.fetch_news(ticker)
+                for article in news_articles:
+                    text = article['summary']
+                    vader_score = sentiment_analysis.analyze_vader(text)
+                    openai_score = sentiment_analysis.analyze_openai(text)
+
+                    # Accumulate sentiment scores
+                    vader_score_sum += vader_score
+                    openai_score_sum += openai_score
+                    num_articles += 1
+
+                    # Save the sentiment scores
+                    database.save_sentiment(ticker, vader_score, openai_score)
+
+                # Calculate average sentiment scores
+                vader_score_avg = vader_score_sum / num_articles if num_articles > 0 else 0
+                openai_score_avg = openai_score_sum / num_articles if num_articles > 0 else 0
+
+                # Calculate potential upside
+                recent_price = prices[-1] if prices else 0
+                historical_price_max = max(prices) if prices else 0
+                potential_upside = historical_price_max - recent_price
+
+                # Placeholder for final score calculation
+                final_score = None  # TODO: Replace with actual calculation
+
+                # Save the final score
+                # TODO: Implement method to save final score in the database
+
+            except Exception as e:
+                print(f"An error occurred while processing ticker {ticker}: {str(e)}")
+
+if __name__ == "__main__":
+    main()
