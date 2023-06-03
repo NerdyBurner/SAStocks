7f15177700d61b55ab81be1b11b05ce8b87d09a9
NerdyBurner <133992676+NerdyBurner@users.noreply.github.com>
2023-06-03 13:26:15 -0600

    Update api.py
    Corrected RSI and MACD indicator pulls
    
    Signed-off-by: NerdyBurner <133992676+NerdyBurner@users.noreply.github.com>

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-
+#Build as of 6.3.23
 import requests
 from config import Config
 from polygon import RESTClient
@@ -7,6 +7,7 @@ class Api:
     def __init__(self, polygon_api_key: str, tickers_file: str):
         self.polygon_api_key = polygon_api_key
         self.tickers_file = tickers_file
+        
 
     def fetch_tickers(self) -> list:
         try:
@@ -19,7 +20,7 @@ class Api:
 
     def fetch_news(self, ticker: str) -> dict:
         try:
-            response = requests.get(f'https://api.polygon.io/v1/meta/symbols/{ticker}/news?apiKey={self.polygon_api_key}')
+            response = requests.get(f'https://api.polygon.io/v1/meta/symbols/{ticker}/news?published_utc=5/26/23&apiKey={self.polygon_api_key}')
             response.raise_for_status()
             return response.json()
         except Exception as e:
@@ -27,22 +28,22 @@ class Api:
             return {}
 
     def fetch_rsi(self, ticker: str) -> dict:
-        try:
-            client = RESTClient(self.polygon_api_key)
-            rsi = client.get_rsi(ticker)
-            return rsi
-        except Exception as e:
-            print(f"Error fetching RSI for {ticker}: {str(e)}")
-            return {}
+        url = f"https://api.polygon.io/v1/indicators/rsi/{ticker}?timespan=day&adjusted=true&window=14&series_type=close&order=desc&apiKey={self.polygon_api_key}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            response.raise_for_status()
+
 
     def fetch_macd(self, ticker: str) -> dict:
-        try:
-            client = RESTClient(self.polygon_api_key)
-            macd = client.get_macd(ticker)
-            return macd
-        except Exception as e:
-            print(f"Error fetching MACD for {ticker}: {str(e)}")
-            return {}
+        url = f"https://api.polygon.io/v1/indicators/macd/{ticker}?timespan=day&adjusted=true&short_window=12&long_window=26&signal_window=9&series_type=close&order=desc&apiKey={self.polygon_api_key}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            response.raise_for_status()
+
 
     def fetch_prices(self, ticker: str, start_date: str, end_date: str) -> dict:
         try:
