3463c51023d501739118850785f88536ce676dd5
NerdyBurner <133992676+NerdyBurner@users.noreply.github.com>
2023-06-05 19:59:35 -0600

    Update config.py
    Debugging Updates
    
    
    Signed-off-by: NerdyBurner <133992676+NerdyBurner@users.noreply.github.com>

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 # config.py
 import os
+from dotenv import load_dotenv
+
+# load .env file
+load_dotenv()
 
 class Config:
-    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-    POLYGON_API_KEY = os.getenv('POLYGON_API_KEY')
+    OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
+    POLYGON_API_KEY = os.environ['POLYGON_API_KEY']
     DATABASE_NAME = 'database.sqlite'
     TICKERS_FILE = 'Tickers.csv'
